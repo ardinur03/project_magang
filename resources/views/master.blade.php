36a40c5ab346f9ed69c4120146663fb41b63a2f3
ardinur03 <ardinurinsan03@gmail.com>
2020-02-04 21:31:11 +0700

    mengisi

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -4,11 +4,17 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title></title>
+    <title>Crud Laravel</title>
 </head>
 <body>
-    @section('content')
-        
-    @endsection
+    <header>
+        <h2>Jual beli</h2>
+        <nav>
+            <a href="/unit">Unit</a>
+            <a href="#">Client</a>
+            <a href="#">Marketing</a>
+            <a href="#">Pembelian</a>
+        </nav>
+    </header>
 </body>
 </html>
