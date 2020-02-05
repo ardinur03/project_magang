b47a11fd9d6433b385aae7c109c7c00869d6e159
ardinur03 <ardinurinsan03@gmail.com>
2020-02-05 13:10:08 +0700

    isi metod pindah

--- app/Http/Controllers/unitController.php
+++ app/Http/Controllers/unitController.php
@@ -3,11 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class unitController extends Controller
 {
-    //lempar rute unit->pindah ke view
+    // methode pindah
     public function pindah () {
-        return view ('halaman_unit.unit');
+        //lempar rute unit->pindah ke view
+        // return view ('halaman_unit.unit');
+        // ambil data dari tabel unit
+        $unit = DB::table('unit')->get();
+        // kirim data ke view halaman_unit/unit
+        return view ('halaman_unit.unit', ['unit' =>$unit]);
     }
+
+   
 }
