0bab30269037d8862bf67122fd503cc2260aa9ac
ardinur03 <ardinurinsan03@gmail.com>
2020-02-05 13:11:11 +0700

    isi konten unit

--- resources/views/halaman_unit/unit.blade.php
+++ resources/views/halaman_unit/unit.blade.php
@@ -6,6 +6,32 @@
 
 {{--  isi bagian konten  --}}
 @section('konten')
-    Unit
     <a href="/home">kembali ke home</a>
+    <div class="container">
+    <a href="/unit/tambah">+ unit</a>
+    <br>
+    <table border="1">
+        <tr>
+            <td>Kode Unit</td>
+            <td>Harga</td>
+            <td>Luas</td>
+            <td>Luas Tanah</td>
+            <td>Keterangan</td>
+            <td>Opsi</td>
+        </tr>
+        @foreach ($unit as $u)
+        <tr>
+            <td>{{ $u->kode_unit }}</td>
+            <td>{{ $u->harga }}</td>
+            <td>{{ $u->luas }}</td>
+            <td>{{ $u->luas_tanah }}</td>
+            <td>{{ $u->ket }}</td>
+            <td>
+                <a href="/unit/edit">edit</a>
+                <a href="/unit/hapus">hapus</a>
+            </td>
+        </tr>
+        @endforeach
+    </table>
+    </div>
 @endsection
