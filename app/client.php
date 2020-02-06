b35f7807443f3f6ecc358fedf9dce7ad70f4acfb
ardinur03 <ardinurinsan03@gmail.com>
2020-02-06 16:50:02 +0700

    menghandle tabel client dan filteryang maudi input

--- app/client.php
+++ app/client.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class client extends Model
 {
-    //
+    //menghandle table cllients jadi client
+    protected $table = "client";
+
+    protected $fillable = ['nama_client', 'tempat_lahir', 'tanggal_lahir', 'jenis_kelamin', 'alamat', 'no_ktp', 'no_hp', 'email', 'ket', 'crated_by'];
 }
