074a8512e366e5ca53cc5a9fdd6d67a3c7839b43
ardinur03 <ardinurinsan03@gmail.com>
2020-02-06 14:52:56 +0700

    rombak dikit

--- resources/views/halaman_unit/unit.blade.php
+++ resources/views/halaman_unit/unit.blade.php
@@ -1,37 +1,54 @@
 {{--  menghubungkan dengan konten view di master  --}}
 @extends('master')
 
-{{--  isi bagian judul  --}}
-@section ('halaman_judul', 'Ini Unit')
+{{--  isi bagian title halaman unit --}}
+@section ('title', 'Unit')
+
 
 {{--  isi bagian konten  --}}
-@section('konten')
-    <a href="/home">kembali ke home</a>
+@section('content')
     <div class="container">
-    <a href="/home/unit/tambah_unit">+ unit</a>
-    <br>
-    <table border="1">
-        <tr>
-            <td>Kode Unit</td>
-            <td>Harga</td>
-            <td>Luas</td>
-            <td>Luas Tanah</td>
-            <td>Keterangan</td>
-            <td>Opsi</td>
-        </tr>
-        @foreach ($unit as $u)
-        <tr>
-            <td>{{ $u->kode_unit }}</td>
-            <td>{{ $u->harga }}</td>
-            <td>{{ $u->luas }}</td>
-            <td>{{ $u->luas_tanah }}</td>
-            <td>{{ $u->ket }}</td>
-            <td>
-                <a href="/home/unit/edit/{{ $u->id_unit }}">edit</a>
-                <a href="/home/unit/hapus/{{ $u->id_unit }}">hapus</a>
-            </td>
-        </tr>
-        @endforeach
-    </table>
+        <div class="row">
+            <div class="col-md-12 col-md-offset-0">
+                <div class="panel panel-default">
+                    <div class="panel-heading">Unit</div>
+                    <div class="panel-body">
+                        {{--  @if (session('status'))
+                            <div class="alert alert-success">
+                                {{ session('status') }}
+                            </div>
+                        @endif    --}}
+                        <a href="/home/unit/tambah_unit" class="btn btn-success">Tambah unit</a>
+                        <br>
+                        <br>
+                        <table  class="table table-bordered table-hover table-striped">
+                            <tr>
+                                <td>Kode Unit</td>
+                                <td>Harga</td>
+                                <td>Luas</td>
+                                <td>Luas Tanah</td>
+                                <td>Keterangan</td>
+                                <td colspan="2">Opsi</td>
+                            </tr>
+                            @foreach ($unit as $u)
+                            <tr>
+                                <td>{{ $u->kode_unit }}</td>
+                                <td>{{ $u->harga }}</td>
+                                <td>{{ $u->luas }}</td>
+                                <td>{{ $u->luas_tanah }}</td>
+                                <td>{{ $u->ket }}</td>
+                                <td>
+                                    <a href="/home/unit/edit/{{ $u->id_unit }}" class="btn btn-warning">edit</a>
+                                </td>
+                                <td>
+                                    <a href="/home/unit/hapus/{{ $u->id_unit }}" class="btn btn-danger">hapus</a>
+                                </td>
+                            </tr>
+                            @endforeach
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
