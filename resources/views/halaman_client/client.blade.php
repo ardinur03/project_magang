14073f1e9b94b070e00b0107cf737a331d5b1725
ardinur03 <ardinurinsan03@gmail.com>
2020-02-06 16:51:19 +0700

    edit judul

--- resources/views/halaman_client/client.blade.php
+++ resources/views/halaman_client/client.blade.php
@@ -10,7 +10,7 @@
     <div class="row">
         <div class="col-md-12 col-md-offset-0">
             <div class="panel panel-default">
-                <div class="panel-heading">Client</div>
+                <div class="panel-heading"><strong>Client</strong></div>
     
                 <div class="panel-body">
                     {{--  @if (session('status'))
