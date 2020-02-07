856fcab26fab87689cb909684745be90ddca4c38
ardinur03 <ardinurinsan03@gmail.com>
2020-02-07 14:49:22 +0700

    rombak edit unit

--- resources/views/halaman_unit/edit_unit.blade.php
+++ resources/views/halaman_unit/edit_unit.blade.php
@@ -1,50 +1,89 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Edit | Unit</title>
-</head>
-<body>
-    <a href="/home/unit">Kembali</a>
-    <div class="container">
-    @foreach($unit as $u)
-    <form action="/home/unit/update" method="POST">
-        {{ csrf_field() }}
-        <input type="hidden" name="id" value="{{ $u->id_unit }}">
-        <table>
-        <tr>
-            <td>Kode Unit</td>
-            <td>:</td>
-            <td><input type="text" name="kode_unit" required="required" value="{{ $u->kode_unit }}"></td>
-        </tr>
-        <tr>
-            <td>Harga</td>
-            <td>:</td>
-            <td><input type="number" name="harga" required="required" value="{{ $u->harga }}"></td>
-        </tr>
-        <tr>
-            <td>Luas</td>
-            <td>:</td>
-            <td><input type="text" name="luas" required="required" value="{{ $u->luas }}"></td>
-        </tr>
-        <tr>
-            <td>Luas Tanah</td>
-            <td>:</td>
-            <td><input type="text" name="luas_tanah" required="required" value="{{ $u->luas_tanah }}"></td>
-        </tr>
-        <tr>
-            <td>Keterangan</td>
-            <td>:</td>
-            <td><textarea name="ket" required="required">{{ $u->ket }}</textarea></td>
-        </tr>
-        <tr>
-        <td><input type="submit" value="Simpan Data"></td>
-        </tr>
-        </table>
-    </form>
-    @endforeach
+@extends('master')
+
+@section('title', 'Edit | Unit')
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-12 col-md-offset-0">
+            <div class="panel panel-default">
+                <div class="panel-heading"><strong>Edit Unit</strong></div>
+
+                <div class="panel-body bg-success">
+                    {{--  @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif    --}}
+
+                    <a href="/home/unit" class="btn btn-warning">Kembali</a>
+                    <div class="card-body">
+                    <form action="/home/unit/proses/{{ $unit->id_unit }}" method="POST">
+                        {{ csrf_field() }}
+                        {{ method_field('PUT') }}
+
+                        {{--  form isi kode unit  --}}
+                        <div class="form-group">
+                            <label>Kode Unit</label>
+                            <input type="text" name="kode_unit" class="form-control" placeholder="Kode Unit..." value="{{ $unit->kode_unit }}">
+                            @if($errors->has('kode_unit'))
+                                <div class="text-danger">
+                                    {{ $errors->first('kode_unit', 'Kode unit harus terisi!!!')}}
+                                </div>
+                            @endif
+                        </div>
+
+                        {{--  form isi harga  --}}
+                        <div class="form-group">
+                            <label>Harga</label>
+                            <input type="number" name="harga" class="form-control" placeholder="Harga..." value="{{ $unit->harga }}">
+                            @if($errors->has('harga'))
+                                <div class="text-danger">
+                                    {{ $errors->first('harga', 'Harga harus terisi!!!')}}
+                                </div>
+                            @endif
+                        </div>
+
+                        {{--  form isi Luas  --}}
+                        <div class="form-group">
+                            <label>Luas</label>
+                            <input type="text" name="luas" class="form-control" placeholder="Luas..." value="{{ $unit->luas }}">
+                            @if($errors->has('luas'))
+                                <div class="text-danger">
+                                    {{ $errors->first('luas', 'Luas harus terisi!!!')}}
+                                </div>
+                            @endif
+                        </div>
+
+                        {{--  form isi luas tanah  --}}
+                        <div class="form-group">
+                            <label>Luas Tanah</label>
+                            <input type="text" name="luas_tanah" class="form-control" placeholder="Luas..." value="{{ $unit->luas_tanah }}">
+                            @if($errors->has('luas_tanah'))
+                                <div class="text-danger">
+                                    {{ $errors->first('luas_tanah', 'Luas Tanah harus terisi!!!')}}
+                                </div>
+                            @endif
+                        </div>
+
+                        {{--  form isi ket  --}}
+                        <div class="form-group">
+                            <label>Keterangan</label>
+                            <textarea name="ket" class="form-control" placeholder="Keterangan...">{{ $unit->ket }}</textarea>
+                            @if($errors->has('ket'))
+                                <div class="text-danger">
+                                    {{ $errors->first('ket', 'Keterangan harus terisi!!!')}}
+                                </div>
+                            @endif
+                        </div>
+                        
+                        <div class="form-group">
+                            <input type="submit" class="btn btn-success" value="Simpan">
+                        </div>
+                    </form>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
-</body>
-</html>
+</div>
+@endsection
