56bbb3b572c936a4313f1fcc7a9a6f22e153ad0b
ardinur03 <ardinurinsan03@gmail.com>
2020-02-07 14:46:29 +0700

    membuat model unit dan mengisi

---
+++ app/Unit.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Unit extends Model {
+    //
+    protected $table = 'unit';
+    // filter yang mana yang mau di inputkan
+    protected $fillable =['kode_unit', 'harga', 'luas', 'luas_tanah', 'ket'];
+    // 
+    protected $primaryKey = 'id_unit';
+    protected $kode_unit ='kode_unit';
+}
