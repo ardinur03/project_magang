e7cf67a9d8c594735bd8f00c035b3a2ddabddcd3
ardinur03 <ardinurinsan03@gmail.com>
2020-02-07 14:47:59 +0700

    membuat controller crud unit

--- app/Http/Controllers/unitController.php
+++ app/Http/Controllers/unitController.php
@@ -3,63 +3,80 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+
+use App\Unit;
 
 class unitController extends Controller
 {
     // methode pindah ke unit dan menampilkan data dari database
     public function pindah () {
         // ambil data dari tabel unit
-        $unit = DB::table('unit')->get();
-        //lempar rute unit->pindah ke view
+        $unit = Unit::all();
         // kirim data ke view halaman_unit/unit
         return view ('halaman_unit.unit', ['unit' =>$unit]);
     }
+
 
     // metod tambah unit
     public function tambah () {
         // halaman view tambah unit
         return view ('halaman_unit.tambah_unit');
     }
-    // metod store untuk mengirim data unit
-    public function store (Request $request) {
-        // kirim data ke tabel unit
-        DB::table('unit')->insert([
+    // metod proses untuk mengirim data unit
+    public function proses (Request $request) {
+        // memvalidasikan form
+        $this->validate($request, [
+            'kode_unit' => 'required',
+            'harga'     => 'required',
+            'luas'      => 'required',
+            'luas_tanah'=> 'required',
+            'ket'       => 'required'
+        ]);
+        // proses input
+        Unit::create([
             'kode_unit' => $request->kode_unit,
-            'harga' => $request->harga,
-            'luas' => $request->luas,
-            'luas_tanah' => $request->luas_tanah,
-            'ket' => $request->ket
+            'harga'     => $request->harga,
+            'luas'      => $request->luas,
+            'luas_tanah'=> $request->luas_tanah,
+            'ket'       => $request->ket
         ]);
         // mengembalikkan
         return redirect ('/home/unit');
     }
 
 
-    // metod edit 
-    public function edit ($id){
-        // mengambil data unit berdasarkan unit yg di pilih
-        $unit = DB::table('unit')->where('id_unit',$id)->get();
-        // passing data unit yang didapat ke view edit_unit.blade.php
-	    return view('halaman_unit.edit_unit',['unit' => $unit]);
+    // metod rute edit 
+    public function edit ($id_unit){
+        $unit = Unit::find($id_unit);
+        return view ('halaman_unit.edit_unit', ['unit'=>$unit]);
     }
-    // proses update
-    public function update (Request $request) {
-        DB::table('unit')->where('id_unit', $request->id)->update([
-            'kode_unit' => $request->kode_unit,
-            'harga' => $request->harga,
-            'luas' => $request->luas,
-            'luas_tanah' => $request->luas_tanah,
-            'ket' => $request->ket
+    // proses edit data
+    public function update ($id_unit, Request $request) {
+        // memvalidasi form edit unit
+        $this->validate($request, [
+            'kode_unit' => 'required',
+            'harga'     => 'required',
+            'luas'      => 'required',
+            'luas_tanah'=> 'required',
+            'ket'       => 'required'
         ]);
+        // proses edit unit
+        $unit = Unit::find($id_unit);
+        $unit->kode_unit = $request->kode_unit;
+        $unit->harga = $request->harga;
+        $unit->luas = $request->luas;
+        $unit->luas_tanah = $request->luas_tanah;
+        $unit->ket = $request->ket;
+        $unit->save();
         // mengembalikkan
         return redirect ('/home/unit');
     }
 
 
     // metod hapus
-    public function hapus ($id) {
-        DB::table('unit')->where('id_unit', $id)->delete();
+    public function delete ($id_unit) {
+       $unit = Unit::find($id_unit);
+       $unit->delete();
        // mengembalikkan
        return redirect ('/home/unit');
     }
