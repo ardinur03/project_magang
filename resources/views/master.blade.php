e55c07e4eb65468eebad676aeb8c4478b5266442
ardinur03 <ardinurinsan03@gmail.com>
2020-02-10 11:06:02 +0700

    memperbaiki tampilan tambah bootstrap

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -49,16 +49,17 @@
 
                     <!-- Right Side Of Navbar -->
                     <ul class="nav navbar-nav navbar-right">
-                        <a href="/home">Home /</a>
-                        <a href="/home/unit">Unit /</a>
-                        <a href="/home/client">Client /</a>
-                        <a href="/home/marketing">Marketing /</a>
-                        <a href="#">Pembelian</a>
+                        
                         <!-- Authentication Links -->
                         @if (Auth::guest())
                             <li><a href="{{ route('login') }}">Login</a></li>
                             <li><a href="{{ route('register') }}">Register</a></li>
                         @else
+                        <li><a href="/home">Home</a></li>
+                        <li><a href="/home/unit">Unit</a></li>
+                        <li><a href="/home/client">Client</a></li>
+                        <li><a href="/home/marketing">Marketing</a></li>
+                        <li><a href="#">Pembelian</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
@@ -77,8 +78,21 @@
                                         </form>
                                     </li>
                                 </ul>
+                                {{-- <ul class="nav navbar-nav navbar-right">
+                                <li><a href="#">Link</a></li>
+                                <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                                <ul class="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li role="separator" class="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                </ul> --}}
+                                
                             </li>
                         @endif
+                        
                     </ul>
                 </div>
             </div>
