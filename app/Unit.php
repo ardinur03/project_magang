1081dd78cd75f376e749031aa118399651b6565f
ardinur03 <ardinurinsan03@gmail.com>
2020-02-11 16:17:21 +0700

    komentar kode unit

--- app/Unit.php
+++ app/Unit.php
@@ -8,8 +8,12 @@ class Unit extends Model {
     //
     protected $table = 'unit';
     // filter yang mana yang mau di inputkan
-    protected $fillable =['kode_unit', 'harga', 'luas', 'luas_tanah', 'ket'];
+    protected $fillable = ['kode_unit', 'harga', 'luas', 'luas_tanah', 'ket'];
     // 
     protected $primaryKey = 'id_unit';
-    protected $kode_unit ='kode_unit';
+    // protected $kode_unit = 'kode_unit';
+
+    // public function client () {
+    //     return this
+    // }
 }
