a7c87a31274d4820a416446a4f0da7c461983769
ardinur03 <ardinurinsan03@gmail.com>
2020-02-11 09:48:02 +0700

    membuat Breadcrumbs edit unit

--- resources/views/halaman_unit/edit_unit.blade.php
+++ resources/views/halaman_unit/edit_unit.blade.php
@@ -6,16 +6,18 @@
     <div class="row">
         <div class="col-md-12 col-md-offset-0">
             <div class="panel panel-default">
-                <div class="panel-heading"><strong>Edit Unit</strong></div>
-
                 <div class="panel-body bg-success">
                     {{--  @if (session('status'))
                         <div class="alert alert-success">
                             {{ session('status') }}
                         </div>
                     @endif    --}}
-
-                    <a href="{{ route('index_unit') }}" class="btn btn-warning">Kembali</a>
+                    <div class="panel-heading">
+                        <ol class="breadcrumb">
+                            <li><a href="{{ route('index_unit') }}">Unit</a></li>
+                            <li class="active">Edit Unit</li>
+                        </ol>
+                    </div>
                     <div class="card-body">
                     <form action="/home/unit/proses/{{ $unit->id_unit }}" method="POST">
                         {{ csrf_field() }}
