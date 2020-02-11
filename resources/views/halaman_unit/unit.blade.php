372d4d6534d93bdfce5ef89d9f0f07088b0443a6
ardinur03 <ardinurinsan03@gmail.com>
2020-02-11 16:21:12 +0700

    tambah judul, tambah nomor di tabel dan mtd link

--- resources/views/halaman_unit/unit.blade.php
+++ resources/views/halaman_unit/unit.blade.php
@@ -11,13 +11,13 @@
         <div class="row">
             <div class="col-md-12 col-md-offset-0">
                 <div class="panel panel-default">
-                    
                     <div class="panel-body bg-success">
                         {{--  @if (session('status'))
                             <div class="alert alert-success">
                                 {{ session('status') }}
                             </div>
                         @endif    --}}
+                        <div class="pull-left"><b>Data Unit</b></div><br>
                         <div class="panel-heading">
                             <ol class="breadcrumb">
                                 <li class="active">Unit</li>
@@ -26,8 +26,9 @@
                         <a href="{{ route('tambah_unit') }}" class="btn btn-success">Tambah unit</a>
                         <br>
                         <br>
-                        <table  class="table table-bordered table-hover table-striped">
+                        <table class="table table-bordered table-hover table-striped">
                             <tr>
+                                <th>No</th>
                                 <th>Kode Unit</th>
                                 <th>Harga</th>
                                 <th>Luas</th>
@@ -35,15 +36,16 @@
                                 <th>Keterangan</th>
                                 <th colspan="2">Opsi</th>
                             </tr>
-                            @foreach ($unit as $u)
+                            @foreach ($unit as $no => $u)
                             <tr>
+                                <td>{{ $no+1 }}</td>
                                 <td>{{ $u->kode_unit }}</td>
                                 <td>{{ $u->harga }}</td>
                                 <td>{{ $u->luas }}</td>
                                 <td>{{ $u->luas_tanah }}</td>
                                 <td>{{ $u->ket }}</td>
                                 <td>
-                                    <a href="/home/unit/edit/{{ $u->id_unit }}" class="btn btn-warning">edit</a>
+                                    <a href="{{ route('edit_unit', $u->id_unit) }}" class="btn btn-warning">edit</a>
                                 </td>
                                 <td>
                                     <a href="/home/unit/hapus/{{ $u->id_unit }}" class="btn btn-danger">hapus</a>
