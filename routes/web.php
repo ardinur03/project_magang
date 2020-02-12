f9c2b0ab2575ea214f598ca683fc405f9a6f0666
ardinur03 <ardinurinsan03@gmail.com>
2020-02-12 16:51:31 +0700

    tambah nama di tiap rute dan buat rute penjualan

--- routes/web.php
+++ routes/web.php
@@ -22,14 +22,14 @@ Route::get('/home', 'HomeController@index')->name('home');
 // 1. crud unit
 // rute halaman unit
 Route::get('/home/unit', 'unitController@pindah')->name('index_unit');
-//crud tampil unit 
+//rute view tambah unit 
 Route::get('/home/unit/tambah_unit', 'unitController@tambah')->name('tambah_unit');
 // rute proses unit
-Route::put('/home/unit/proses/{id_unit}', 'unitController@update');
-// rute edit unit
+Route::put('/home/unit/proses/{id_unit}', 'unitController@update')->name('proses_edit_unit');
+// rute view edit unit
 Route::get('/home/unit/edit/{id_unit}', 'unitController@edit')->name('edit_unit');
-// rute tambah
-Route::post('/home/unit/proses', 'unitController@proses')->name('proses_tambah');
+// rute tambah view
+Route::post('/home/unit/proses', 'unitController@proses')->name('proses_tambah_unit');
 // rute hapus unit
 Route::get('/home/unit/hapus/{id_unit}', 'unitController@delete')->name('hapus_unit');
 
@@ -42,23 +42,33 @@ Route::get('/home/client/tambah', 'clientController@tambah')->name('client_tamba
 // rute proses simpan client
 Route::post('/home/client/proses', 'clientController@proses')->name('proses_simpan');
 // rute hapus client
-Route::get('/home/client/hapus/{id_client}', 'clientController@delete');
+Route::get('/home/client/hapus/{id_client}', 'clientController@delete')->name('hapus_client');
 // rute edit client
-Route::get('/home/client/edit/{id_client}', 'clientController@edit');
+Route::get('/home/client/edit/{id_client}', 'clientController@edit')->name('edit_client');
 // rute proses update
-Route::put('/home/client/update/{id_client}', 'clientController@update');
+Route::put('/home/client/update/{id_client}', 'clientController@update')->name('proses_edit');
 
 
 // 3. crud marketing
 // rute halaman marketing indexnya
 Route::get('/home/marketing', 'marketingController@index')->name('index_marketing');
 // rute hapus data marketing
-Route::get('/home/marketing/hapus/{id_marketing}', 'marketingController@delete');
+Route::get('/home/marketing/hapus/{id_marketing}', 'marketingController@delete')->name('hapus_marketing');
 // rute tambah marketing
 Route::get('/home/marketing/tambah_marketing', 'marketingController@tambah')->name('tambah_marketing');
 // rute proses inputan 
 Route::post('/home/marketing/proses', 'marketingController@proses')->name('proses_tambah');
-// rute edit marketing
-Route::get('/home/marketing/edit/{id_marketing}', 'marketingController@edit');
+// rute edit marketing view
+Route::get('/home/marketing/edit/{id_marketing}', 'marketingController@edit')->name('edit_marketing');
 // rute proses marketing
-Route::put('/home/marketing/update/{id_marketing}', 'marketingController@update');
+Route::put('/home/marketing/update/{id_marketing}', 'marketingController@update')->name('proses_update');
+
+
+
+//Pembelian 
+Route::get('/pembelian', 'pembelianController@index')->nama('index_pembelian');
+
+
+
+// tambahan doang
+Route::get('/home/coba', 'homeController@coba');
