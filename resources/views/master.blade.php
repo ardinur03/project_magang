a61440c239abb53249e42c4a8ac12804321b5b78
ardinur03 <ardinurinsan03@gmail.com>
2020-02-12 17:09:30 +0700

    menyambungkan link pembelian

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -59,7 +59,7 @@
                         <li><a href="{{ route('index_unit') }}">Unit</a></li>
                         <li><a href="{{ route('index_client') }}">Client</a></li>
                         <li><a href="{{ route('index_marketing') }}">Marketing</a></li>
-                        <li><a href="#">Pembelian</a></li>
+                        <li><a href="{{ route('index_pembelian') }}">Pembelian</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
