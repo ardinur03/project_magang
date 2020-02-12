5e1425dd9de2db68ddeb5c8c956439b7d865fed4
ardinur03 <ardinurinsan03@gmail.com>
2020-02-12 15:50:10 +0700

    buat fungsi belongsTo client dan marketing

--- app/User.php
+++ app/User.php
@@ -26,4 +26,16 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    public function client()
+    {
+
+        return $this->belongsTo('App\Client', 'id_client');
+    }
+
+    public function marketing()
+    {
+
+        return $this->belongsTo('App\Marketing', 'id_marketing');
+    }
 }
