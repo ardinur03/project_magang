ffb038290aee49bd1de5ba91e21064683e4bc1ad
ardinur03 <ardinurinsan03@gmail.com>
2020-02-14 09:24:42 +0700

    menyambungkan unit ke pembelian belongsto

--- app/Unit.php
+++ app/Unit.php
@@ -11,9 +11,15 @@ class Unit extends Model {
     protected $fillable = ['kode_unit', 'harga', 'luas', 'luas_tanah', 'ket'];
     // 
     protected $primaryKey = 'id_unit';
-    // protected $kode_unit = 'kode_unit';
+    protected $kode_unit = 'kode_unit';
 
     // public function client () {
     //     return this
     // }
+
+    // untuk menyambungkan dengan pembelian
+    public function pembelian()
+    {
+        return $this->belongsTo('App\Pembelian', 'id_pembelian');
+    }
 }
