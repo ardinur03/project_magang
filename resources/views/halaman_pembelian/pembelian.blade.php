2bacbdd34e993dc166beaefd14e95b7f09056ecc
ardinur03 <ardinurinsan03@gmail.com>
2020-02-14 09:20:23 +0700

    isi file pembelian sampai muuncul dari db ke view

--- resources/views/halaman_pembelian/pembelian.blade.php
+++ resources/views/halaman_pembelian/pembelian.blade.php
@@ -12,34 +12,43 @@
                             {{ session('status') }}
                         </div>
                     @endif   --}}
-                    <div class="pull-left"><b>Penjualan</b></div><br>
+                    <div class="pull-left"><b>Pembelian</b></div><br>
                     <div class="panel-heading">
                         <ol class="breadcrumb">
-                            <li class="active">Penjualan</li>
+                            <li class="active">Pembelian</li>
                         </ol>
                     </div>
                     <a href="/home" class="btn btn-warning">Kembali Ke Home</a>
-                    <form action="" method="POST">
+                    <form action="{{ route('proses_simpan') }}" method="POST">
                         {{-- {{ crsf_field() }} --}}
 
                         {{-- pilih unit --}}
                         <div class="form-group">
                             <label>Pilih Unit</label>
                             <select name="pilih_unit" class="form-control">
-                                <option  value="" selected>***Pilih Unit***</option>
-                                <option value=""></option>
+                                <option value="" >Isi Unit...</option>
+                                @foreach ($pembelian as $p)
+                                <option value="{{ $p->pilih_unit->kode_unit }}">{{ $p->pilih_unit->kode_unit }}</option>
+                                @endforeach
                             </select>
+                            {{--  validasi  --}}
                             @if ($errors->has('pilih_unit'))
                                 <div class="text-danger">
                                     {{ $errors->first ('pilih_unit','Unit harus terisi!!!') }}
                                 </div>
                             @endif
                         </div>
-
+                        
                         {{-- pilih client --}}
                         <div class="form-group">
                             <label>Pilih Client</label>
-                            <input type="" name="pilih_client" class="form-control" placeholder="">
+                            <select name="pilih_client" class="form-control">
+                                <option value="" selected>Isi Client</option>
+                                @foreach ($pembelian as $p)
+                                <option value="{{ $p->pilih_client->nama_client}}">{{ $p->pilih_client->nama_client }}</option>
+                                @endforeach
+                            </select>
+                            {{--  validasi  --}}
                             @if ($errors->has('pilih_client'))
                                 <div class="text-danger">
                                     {{ $errors->first ('pilih_client','Client harus terisi!!!') }}
@@ -49,8 +58,14 @@
 
                         {{-- pilih marketing --}}
                         <div class="form-group">
-                            <label>Pilihi Marketing</label>
-                            <input type="" name="pilih_marketing" class="form-control" placeholder="">
+                            <label>Pilih Marketing</label>
+                            <select name="pilih_marketing" class="form-control">
+                                <option value="" >Isi Marketing</option>
+                                @foreach ($pembelian as $p)
+                                <option value="{{ $p->pilih_marketing->nama}}">{{ $p->pilih_marketing->nama }}</option>
+                                @endforeach
+                            </select>
+                            {{--  validasi  --}}
                             @if ($errors->has('pilih_marketing'))
                                 <div class="text-danger">
                                     {{ $errors->first ('pilih_marketing','Marketing harus terisi!!!') }}
