7cd120fd0f25741b1f1f1c6960a4bb7961fbe46f
ardinur03 <ardinurinsan03@gmail.com>
2020-02-14 09:24:04 +0700

    membuat fungsi untuk validasi dan kirim data

--- app/Http/Controllers/pembelianController.php
+++ app/Http/Controllers/pembelianController.php
@@ -3,20 +3,30 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Pembelian;
+use App\Unit;
 class pembelianController extends Controller
 {
     //index pembelian
     public function index () {
-        return view ('halaman_pembelian.pembelian');
+        $pembelian = Pembelian::all();
+        return view ('halaman_pembelian.pembelian', ['pembelian' => $pembelian]);
     }
 
-    /*// proses tambah pembelian
-    public function proses () {
+    // proses tambah pembelian <- rute('proses_tambah)
+    public function proses (Request $request) {
         // memvalidasi
         $this->validate($request, [ 
-            
+            'pilih_unit' => 'required',
+            'pilih_client' => 'required',
+            'pilih_marketing' => 'required'
         ]);
-    } */
+        // proses input ke database
+        Pembelian::create([
+            'pilih_unit' => $request->pilih_unit,
+            'pilih_client' => $request->pilih_client,
+            'pilih_marketing' => $request->pilih_marketing,
+        ]);
+    } 
     
 }
