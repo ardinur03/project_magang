e7954baf405237036085d6b7f6751267c2551392
ardinur03 <ardinurinsan03@gmail.com>
2020-02-21 14:51:33 +0700

    hapus metod yang tidak berguna

--- app/Marketing.php
+++ app/Marketing.php
@@ -19,9 +19,5 @@ class Marketing extends Model
         return $this->belongsTo('App\User', 'created_by');
     }
    
-    // untuk tersambung dengan pembelian
-    public function pembelian() {
-        return $this->belongsTo('App\Pembelian', 'id_pembelian');
-    }
     
 }
