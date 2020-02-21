3ec708fed3cb38eac19281109261ea28f426ea46
ardinur03 <ardinurinsan03@gmail.com>
2020-02-21 14:50:58 +0700

    hapus metod yang tidak berguna

--- app/Unit.php
+++ app/Unit.php
@@ -5,21 +5,16 @@ namespace App;
 use Illuminate\Database\Eloquent\Model;
 
 class Unit extends Model {
-    //
+    // menghandle table cllients jadi unit
     protected $table = 'unit';
     // filter yang mana yang mau di inputkan
     protected $fillable = ['kode_unit', 'harga', 'luas', 'luas_tanah', 'ket'];
     // 
     protected $primaryKey = 'id_unit';
-    protected $kode_unit = 'kode_unit';
-
+    
     // public function client () {
     //     return this
     // }
 
-    // untuk menyambungkan dengan pembelian
-    public function pembelian()
-    {
-        return $this->belongsTo('App\Pembelian', 'id_pembelian');
-    }
+    
 }
