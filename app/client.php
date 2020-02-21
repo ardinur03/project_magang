f5d3aad452e2ffd1741999a3b81a6dfc5f961409
ardinur03 <ardinurinsan03@gmail.com>
2020-02-21 14:51:56 +0700

    hapus metod tak berguna

--- app/client.php
+++ app/client.php
@@ -6,19 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class client extends Model {
     //menghandle table cllients jadi client
-    protected $table = "client";
+    protected $table = 'client';
 
     protected $fillable = ['nama_client', 'tempat_lahir', 'tanggal_lahir', 'jenis_kelamin', 'alamat', 'no_ktp', 'no_hp', 'email', 'ket', 'crated_by'];
     
-    // untuk
     protected $primaryKey = 'id_client';
     // 
     public function user_create() {
         return $this->belongsTo('App\User', 'crated_by');
     }
     
-    // untuk tersambung ke pembelian
-    public function pembelian() {
-        return $this->belongsTo('App\Pembelian', 'id_pembelian');
-    }
 }
