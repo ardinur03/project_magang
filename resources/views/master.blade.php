f34608fbef8f96579e55206295c7d2f8c09eb390
ardinur03 <ardinurinsan03@gmail.com>
2020-02-21 14:48:51 +0700

    buat rute list pembelian

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -60,6 +60,7 @@
                         <li><a href="{{ route('index_client') }}">Client</a></li>
                         <li><a href="{{ route('index_marketing') }}">Marketing</a></li>
                         <li><a href="{{ route('index_pembelian') }}">Pembelian</a></li>
+                        <li><a href="{{ route('index_list_pembelian') }}">List Pembelian</a></li>
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
                                     {{ Auth::user()->name }} <span class="caret"></span>
