5ceae99b222f932bb9b9d77ef0dc56648b8d3e3e
ardinur03 <ardinurinsan03@gmail.com>
2020-02-28 11:30:19 +0700

    kecilkan form

--- resources/views/halaman_pembelian/pembelian.blade.php
+++ resources/views/halaman_pembelian/pembelian.blade.php
@@ -4,7 +4,7 @@
 @section('content')
 <div class="container">
     <div class="row">
-        <div class="col-md-12 col-md-offset-0">
+        <div class="col-md-6 col-md-offset-3">
             <div class="panel panel-default">
                 <div class="panel-body">
                 {{--
